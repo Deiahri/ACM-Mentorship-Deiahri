9515666a7de147a58cae33ab4cb6cef210952a41
Junda Yin <junda.yin1@gmail.com>
2025-02-15 19:30:08 -0600

    Fixed Tailwind build issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./tailwind_out.css";
 import AppRouting from "./AppRouting.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 
