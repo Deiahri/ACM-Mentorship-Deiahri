8153e9b3f2351c3639f8635f3f824901b6321dfb
Junda Yin <junda.yin1@gmail.com>
2025-02-25 15:49:06 -0600

    Fixed social types error on backend

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -5,14 +5,25 @@ export type SocialType =
   | "twitter"
   | "youtube"
   | "linkedIn"
-  | "discord";
+  | "discord"
+  | "github"
+  | "stackOverflow"
+  | "hackerrank"
+  | "facebook"
+  | "portfolio";
 export const SocialTypes = [
   "instagram",
   "twitter",
   "youtube",
   "linkedIn",
   "discord",
+  "github",
+  "stackOverflow",
+  "hackerrank",
+  "facebook",
+  "portfolio"
 ];
+
 export type SocialObj = {
   type: SocialType;
   url: string;
